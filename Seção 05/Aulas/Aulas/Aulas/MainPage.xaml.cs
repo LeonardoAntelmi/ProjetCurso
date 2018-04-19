b830f8bbcb5b500b7bea7007346569bd3b99e23c
Leonardo <leonardo.antelmi@tantto.com.br>
2018-04-19 11:47:49 -0300

    App1 concluido

---
+++ Seção 05/Aulas/Aulas/Aulas/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Aulas.Servico.Modelo;
+using Aulas.Servico;
+
+namespace Aulas
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;
+		}
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+
+            //To do - Validações
+
+            string cep = CEP.Text.Trim();
+            if(Validacao(cep))
+            {
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+                try
+                {
+                    RESULTADO.Text = string.Format("Endereço: {0}, {1}, {2}, {3}", end.logradouro, end.bairro, end.localidade, end.uf);
+                }
+                catch
+                {
+                    DisplayAlert("ERRO CRÍTICO!", "Verifique sua conexao com a internet e tente novamente.", "Ok");
+                }
+            }
+        }
+
+        private bool Validacao(string cep)
+        {
+            bool validade = true;
+            if (cep.Length != 8)
+            {
+                DisplayAlert("ERRO!", "O CEP deve conter 8 digitos!", "Ok");
+                validade = false;
+            }
+            int novoCep = 0;
+            if (!int.TryParse(cep, out novoCep))
+            {
+                DisplayAlert("ERRO!", "O CEP deve conter apenas números", "Ok");
+                validade = false;
+            }
+            if(ViaCEPServico.BuscarEnderecoViaCEP(cep).logradouro == null)
+            {
+                DisplayAlert("ERRO!", "CEP não encontrado", "Ok");
+                validade = false;
+            }
+
+
+            return validade;
+        }
+	}
+}
---
+++ Seção 05/Aulas/Aulas/Aulas/Servico/ViaCEPServico.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using Aulas.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace Aulas.Servico
+{
+    class ViaCEPServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            
+            return end;
+            
+        }
+
+    }
+}
